098f5965ce4bcdba5d81c35f826c655d10a954e0
Stephen Garlick <sgarlick987@gmail.com>
2016-10-24 16:31:17 -0400

    remove vpc name output

--- network.tf
+++ network.tf
@@ -36,10 +36,6 @@ output "vpc_id" {
   value = "${module.vpc.vpc_id}"
 }
 
-output "vpc_name" {
-  value = "${module.vpc.name}"
-}
-
 output "private_subnets" {
   value = "${module.vpc.private_subnets}"
 }
